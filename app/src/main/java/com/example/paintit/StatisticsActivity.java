3aaf09a2e52ee38300b59fa7b3d59ab7f197fbfd
Omriz32 <omri.zohar@leyada.net>
2023-04-18 21:28:58 +0300

    dd

--- app/src/main/java/com/example/paintit/StatisticsActivity.java
+++ app/src/main/java/com/example/paintit/StatisticsActivity.java
@@ -2,8 +2,10 @@ package com.example.paintit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -11,7 +13,7 @@ public class StatisticsActivity extends AppCompatActivity {
 
     Button button;
     MediaPlayer mediaPlayer;
-
+    Intent intent;
     TextView squares_value, time_value;
 
     @Override
@@ -19,8 +21,21 @@ public class StatisticsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
 
+        button = findViewById(R.id.home_button);
+        intent = new Intent(StatisticsActivity.this, GalleryActivity.class);
         squares_value = findViewById(R.id.squares_value);
         time_value = findViewById(R.id.time_value);
         mediaPlayer = MediaPlayer.create(this, R.raw.button_click);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+                releaseInstance();
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
