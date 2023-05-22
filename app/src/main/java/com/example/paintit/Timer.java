7f7585feea1cb6b212de18b6dbfe3c598c79fdf4
Magal-Project <magal.ofir@leyada.net>
2023-05-22 20:14:37 +0300

    Update Timer.java

--- app/src/main/java/com/example/paintit/Timer.java
+++ app/src/main/java/com/example/paintit/Timer.java
@@ -1,2 +1,40 @@
-package com.example.paintit;public class Timer {
+package com.example.paintit;
+
+import java.util.Date;
+
+public class Timer {
+    Date start;
+    Date end;
+    public Timer (Date start, Date end){
+        this.start = start;
+        this.end = end;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+    public long diffrence (){
+        long diff = end.getTime() - start.getTime();
+        return diff;
+    }
+    public long[] format (long diff){
+        long[] time = new long[4];
+        time[0] = diff/1000%60;
+        time[1] = diff/60000%60;
+        time[2] = diff/3600000%24;
+        time[3] = diff/86400000;
+        return time;
+    }
 }
